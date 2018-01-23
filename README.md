2d132b9b6f428bfa73254de53535aa419040910f
arrickx <24638167+arrickx@users.noreply.github.com>
2018-01-22 22:37:36 -0500

    update

--- README.md
+++ README.md
@@ -1,1 +1,34 @@
-# BadmintonScoreCounter
+# Badminton Score Counter
+* A single-screen app that counts badminton scores,
+[![Image1](gif/counter1.gif)](Image1)
+[![Image2](gif/counter2.gif)](Image2)
+[![Image3](gif/counter3.gif)](Image3)
+[![Image4](gif/counter4.gif)](Image4)
+
+## First thing first - Rules of Badminton
+> A match consists of the best of 3 games of 21 points.
+> Every time there is a serve – there is a point scored.
+> The side winning a rally adds a point to its score.
+> At 20 all, the side which gains a 2 point lead first, wins that game.
+> At 29 all, the side scoring the 30th point, wins that game.
+> The side winning a game serves first in the next game.
+
+## Getting Started
+
+* You can *[clone](https://github.com/arrickx/BadmintonScoreCounter.git)* or *[download](https://github.com/arrickx/BadmintonScoreCounter.git)* this project via [GitHub](https://github.com) to your local machine.
+* Download and Install *[Android Studio](https://developer.android.com/studio/index.html)*
+
+### How to use
+
+* Open existing Android Studio Project
+* Use Android Studio emulators to run this app
+* (Or use your Android phone to run this app via Android Studio)
+
+
+## License
+
+* This project is licensed under the MIT License - see the [LICENSE](LICENSE) file for details.
+
+## Acknowledgments
+
+* **Winner Cup Image** was provided by [FREEPIK](https://image.freepik.com/free-vector/hand-holding-winner-cup_23-2147504768.jpg).
--- app/src/main/java/com/example/android/badmintonscorecounter/MainActivity.java
+++ app/src/main/java/com/example/android/badmintonscorecounter/MainActivity.java
@@ -3,13 +3,25 @@ package com.example.android.badmintonscorecounter;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
+/**
+ * This app is a score counter for badminton.
+ * It follows the rules of badminton.
+ * It also automatically switch game and show a winner.
+ */
 public class MainActivity extends AppCompatActivity {
-    int scoreA = 20;
-    int scoreB = 20;
+    /**
+     * Creates values for both team's original score and game number.
+     * It also creates the value for the reset counter.
+     */
+    int scoreA = 0;
+    int scoreB = 0;
     int gameA = 0;
     int gameB = 0;
+    int resetCounter = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +30,9 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    /**
+     * This function displays score of current game.
+     */
     public void displayScore() {
         TextView scoreForTeamA = findViewById(R.id.team_a_score);
         TextView scoreForTeamB = findViewById(R.id.team_b_score);
@@ -25,17 +40,25 @@ public class MainActivity extends AppCompatActivity {
         scoreForTeamB.setText(String.valueOf(scoreB));
     }
 
+    /**
+     * This function add point for Team A and check if there is a winner.
+     */
     public void teamAScore(View view) {
         scoreA += 1;
         checkGameWinner();
     }
 
+    /**
+     * This function add point for Team B and check if there is a winner.
+     */
     public void teamBScore(View view) {
         scoreB += 1;
         checkGameWinner();
     }
 
-
+    /**
+     * This function updates the game table if any of the team win the current game.
+     */
     public void updateTable() {
         TextView gameOneTeamA = findViewById(R.id.game1_a);
         TextView gameOneTeamB = findViewById(R.id.game1_b);
@@ -57,30 +80,75 @@ public class MainActivity extends AppCompatActivity {
         scoreB = 0;
     }
 
+    /**
+     * This function will shows the winner message if any of the team won the match.
+     */
     public void winnerMessage() {
+        TextView winnerTeam = findViewById(R.id.winner_team);
         if (gameA > 1) {
-            return;
+            winnerTeam.setText(R.string.team_a);
+            findViewById(R.id.winner_message).setVisibility(View.VISIBLE);
         }
         if (gameB > 1) {
-            return;
+            winnerTeam.setText(R.string.team_b);
+            findViewById(R.id.winner_message).setVisibility(View.VISIBLE);
         }
     }
 
+    /**
+     * This function checks if any of the team win the game.
+     */
     public void checkGameWinner() {
         if (scoreA > 29) {
             updateTable();
             gameA += 1;
+            winnerMessage();
         } else if (scoreA > 20 && scoreA - scoreB > 1) {
             updateTable();
             gameA += 1;
+            winnerMessage();
         }
         if (scoreB > 29) {
             updateTable();
             gameB += 1;
+            winnerMessage();
         } else if (scoreB > 20 && scoreB - scoreA > 1) {
             updateTable();
             gameB += 1;
+            winnerMessage();
         }
         displayScore();
+    }
+
+    /**
+     * This function resets everything to original state.
+     */
+    public void resetScore(View view) {
+        resetCounter += 1;
+        if (resetCounter > 1) {
+            TextView gameOneTeamA = findViewById(R.id.game1_a);
+            TextView gameOneTeamB = findViewById(R.id.game1_b);
+            TextView gameTwoTeamA = findViewById(R.id.game2_a);
+            TextView gameTwoTeamB = findViewById(R.id.game2_b);
+            TextView gameThreeTeamA = findViewById(R.id.game3_a);
+            TextView gameThreeTeamB = findViewById(R.id.game3_b);
+            scoreA = 0;
+            scoreB = 0;
+            gameA = 0;
+            gameB = 0;
+            displayScore();
+            gameThreeTeamA.setText(String.valueOf(scoreA));
+            gameThreeTeamB.setText(String.valueOf(scoreB));
+            gameTwoTeamA.setText(String.valueOf(scoreA));
+            gameTwoTeamB.setText(String.valueOf(scoreB));
+            gameOneTeamA.setText(String.valueOf(scoreA));
+            gameOneTeamB.setText(String.valueOf(scoreB));
+            findViewById(R.id.winner_message).setVisibility(View.INVISIBLE);
+            resetCounter = 0;
+        } else {
+            //Let the user to confirm for the reset
+            Toast.makeText(this, R.string.reset_confirmation, Toast.LENGTH_SHORT).show();
+        }
+
     }
 }
